4920ea76399f2ba03e7ed48b0b9e29c5c5e8e4f6
AQian <970506352@qq.com>
2023-12-29 11:16:30 +0800

    feat: 实现token存储与携带

--- src/fetch/index.ts
+++ src/fetch/index.ts
@@ -1,9 +1,29 @@
 import originalKy from 'ky';
+
 const ky = originalKy.extend({
   prefixUrl: import.meta.env.VITE_PREFIX_URL,
   timeout: 6000,
   hooks: {
-    afterResponse: [],
+    beforeRequest: [
+      (req) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+          req.headers.set('Authorization', token);
+        }
+      },
+    ],
+    afterResponse: [
+      async (req, options, res) => {
+        if (res.ok) {
+          const clonedRes = res.clone();
+          const data = await clonedRes.json();
+          if (data.token) {
+            localStorage.setItem('token', data.token);
+          }
+        }
+        return res;
+      },
+    ],
   },
 });
 
