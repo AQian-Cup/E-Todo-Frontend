495e8ae96844a7a6ddd10167e9a14c0adaf240e5
AQian <970506352@qq.com>
2024-01-17 10:24:15 +0800

    feat: 添加首页

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,12 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      name: '首页',
+      path: '/',
+      component: () => import('@/views/home.vue'),
+    },
+    {
+      name: '注册/登录',
       path: '/auth',
       component: () => import('@/views/auth.vue'),
     },
---
+++ src/views/home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div h-screen w-full flex justify-start justify-center p-10 space-x-4>
+    <calendar></calendar>
+    <todo-list>
+      <todo-card></todo-card>
+    </todo-list>
+  </div>
+</template>
+<script lang="ts" setup></script>
+<style scoped></style>
