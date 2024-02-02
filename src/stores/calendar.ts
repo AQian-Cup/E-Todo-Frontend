e6c7f3f20fbf33d19d2f0dc4c836d87b63c1ca40
AQian <970506352@qq.com>
2024-02-02 13:57:09 +0800

    refactor: 调整为luxon表述

--- src/stores/calendar.ts
+++ src/stores/calendar.ts
@@ -1,10 +1,11 @@
 import { ref } from 'vue';
 import { defineStore } from 'pinia';
+import { DateTime } from 'luxon';
 
 export const useCalendarStore = defineStore('calendar', () => {
-  const now = ref(new Date());
-  const year = ref(now.value.getFullYear());
-  const month = ref(now.value.getMonth() + 1);
-  const day = ref(now.value.getDate());
+  const now = ref(DateTime.now());
+  const year = ref(now.value.year);
+  const month = ref(now.value.month);
+  const day = ref(now.value.day);
   return { now, year, month, day };
 });
