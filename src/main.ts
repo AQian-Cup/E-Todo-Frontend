d04d30810f40e5b11cd1e4e0c9dbda6d56325b66
AQian <970506352@qq.com>
2024-02-02 13:53:44 +0800

    chore: 调整tailwind样式兼容性

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import App from './App.vue';
 import router from './router';
 import VueDatePicker from '@vuepic/vue-datepicker';
 import 'virtual:uno.css';
-import '@unocss/reset/tailwind.css';
+import '@unocss/reset/tailwind-compat.css';
 import '@/assets/base.css';
 import '@vuepic/vue-datepicker/dist/main.css';
 
